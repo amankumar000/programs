9f2f283d5168d104d2b6025104fa3bde4c022020
amankumar000 <91332159+amankumar000@users.noreply.github.com>
2021-10-01 14:36:30 +0530

    Add files via upload

---
+++ all.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+float x,y,s;
+x=1.1;
+y=2.2;
+s =x+y;
+printf("%f\n",s);
+s =x-y;
+printf("%f\n",s);
+s =x*y;
+printf("%f\n",s);
+s =x/y;
+printf("%f\n",s);
+
+return 0;
+}
